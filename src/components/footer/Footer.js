5312584585d5bf7b6ee9585e9ad828f50b114a82
vikasrathod163 <vikasrathod010603@gmail.com>
2025-04-26 15:53:58 +0530

    footer updated

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with Vikas Rathod")}
+          {emoji("Copyright vikasrathod ©2025 All rights reserved")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
